a884f5d6c9e23644de37adac2f51717e17eec8cb
Ankush <markad.ankush@yahoo.com>
2018-12-20 14:54:20 +0530

    Complete Till Hoisting

---
+++ External.js
@@ -0,0 +1,15 @@
+function postinformation()
+{
+    document.getElementById("first_span").innerHTML = "This is my first javascript Program added in external js file."
+}
+
+
+function getdetails_innertext()
+{
+    alert(document.getElementById("first_span").innerText);
+}
+
+function getdetails_innerhtml()
+{
+    alert(document.getElementById("first_span").innerHTML);
+}
